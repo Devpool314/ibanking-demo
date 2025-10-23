57bcabfc4fcb6a84a94e89ed0a2cfe3513bf7b69
Hung <nohaimai610@hotmail.com>
2025-10-23 23:40:15 +0700

    Delete tuition-system directory

--- tuition-system/src/main/java/com/example/tuition_system/repository/PaymentRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.tuition_system.repository;
-
-import com.example.tuition_system.model.Payment;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface PaymentRepository extends JpaRepository<Payment, Long> {
-}
--- tuition-system/src/main/java/com/example/tuition_system/repository/TuitionInfoRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.tuition_system.repository;
-
-import com.example.tuition_system.model.TuitionInfo;
-import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.Optional;
-
-public interface TuitionInfoRepository extends JpaRepository<TuitionInfo, Long> {
-    Optional<TuitionInfo> findByMssv(String mssv);
-}
