c039e7de7c8c19637ac073a2ccfdb6f99542e6f3
Hung <nohaimai610@hotmail.com>
2025-10-24 00:28:51 +0700

    Add files via upload

---
+++ tuition-system/src/main/java/com/example/tuition_system/repository/PaymentRepository.java
@@ -0,0 +1,11 @@
+package com.example.tuition_system.repository;
+
+import com.example.tuition_system.model.Payment;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PaymentRepository extends JpaRepository<Payment, Long> {
+    boolean existsByCorrelationId(String correlationId);
+
+}
